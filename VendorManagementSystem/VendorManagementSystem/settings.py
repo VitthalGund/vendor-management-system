b11a9e91ba4290ec12870f2602e0fc338e63365b
VitthalGund <vitthalgund30@gmail.com>
2024-04-27 20:33:05 +0530

    authentication app created

--- VendorManagementSystem/VendorManagementSystem/settings.py
+++ VendorManagementSystem/VendorManagementSystem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
+    "authentication",
     "vendor_management",
     "purchase_order_tracking",
 ]
