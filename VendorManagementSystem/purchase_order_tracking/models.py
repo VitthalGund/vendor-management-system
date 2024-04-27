a8a4f1f75df4bf0c4d5c221fbc28072e7b58099e
VitthalGund <vitthalgund30@gmail.com>
2024-04-27 18:29:17 +0530

    PurchaseOrder model created

--- VendorManagementSystem/purchase_order_tracking/models.py
+++ VendorManagementSystem/purchase_order_tracking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from vendor_management.models import Vendor
 
-# Create your models here.
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=100, unique=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_date = models.DateTimeField(null=True, blank=True)
+    actual_delivery_date = models.DateTimeField(null=True, blank=True)
+    items = models.JSONField()
+    quantity = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, default='pending')
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField(auto_now_add=True)
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.po_number
